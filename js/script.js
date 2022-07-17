c3c71203dccbb067c610ea58ed42ea0389dc7223
ozzydo <mee.oguzhan.dogan@gmail.com>
2022-07-17 23:31:39 +0300

    toast, yapildi isareti, element silme eklendi

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+/*******************girilen texti alıp, kelimelerin baş harflerini büyük yapmak için********************/
+
 function newElement()
 {
     const taskDOM = document.querySelector('#task')
@@ -5,6 +8,7 @@ function newElement()
         if(onlySpaces(taskDOM.value)==true) 
         {
             console.log("Yanlış işlem")
+            $('.error').toast('show')
             taskDOM.value=""
         }
         else if(taskDOM.value){
@@ -21,16 +25,53 @@ function newElement()
         }
 }
 
+/******************girilen textin dolu olup olmadığını kontrol etmek için********************/
 
 //içinde bir değer olup olamdığını kontrol ediyor bir değer yoksa "true" çıktısı veriyor
 function onlySpaces(str) {
     return str.trim().length === 0;
 }
 
+/*******************listeye eleman eklemek için************************/
 
 let listDOM = document.querySelector('#list')
 const addItem = (todoList)=>{
     let liDOM = document.createElement("li") // element oluştruldu    
-    liDOM.innerHTML = `${todoList}` // elementin içine yazıldı
-    listDOM.append(liDOM); // element eklendi
+    liDOM.innerHTML = `${todoList} <span class="kapat" onclick="this.parentElement.remove()">x</span>` // elementin içine yazıldı
+    liDOM.addEventListener('click',cizgi)
+    listDOM.append(liDOM) // element eklendi
+    $('.success').toast('show')
+    localStorage.setItem('listInfo', JSON.stringify(liDOM.innerHTML))
+    function cizgi(){
+        let classes = liDOM.classList
+        console.log(classes)
+        classes.value == 'checked' ? liDOM.classList.remove('checked') : liDOM.classList.add('checked')
+    }
 }
+
+/*******************listede var elemanları silmek için*************************/
+
+let closebtns = document.getElementsByClassName("kapat")
+var i
+// Loop through the elements, and hide the parent, when clicked on
+for (i = 0; i < closebtns.length; i++)
+{
+closebtns[i].addEventListener("click", function() {
+    //this.parentElement.style.display = 'none';
+    this.parentElement.remove()
+  });
+}
+
+/*******************listede var olan elemanları yapıldı işaretlemek için********************/
+
+let liDOM2 = document.querySelectorAll('li')
+var j 
+
+for (j = 0; j < liDOM2.length; j++){
+    liDOM2[j].addEventListener("click", function() {
+        console.log(this.classList)
+        let classes = this.classList
+        classes.value == 'checked' ? this.classList.remove('checked') : this.classList.add('checked')
+      });
+}
+/*---------------------------------------------------------------*/
